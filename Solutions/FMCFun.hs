1c6d67bfbdfa6294ccd73554ba130dece97e9489
Augusto <augustocesaaaar@gmail.com>
2025-09-30 20:13:01 -0300

    FMCFun.hs completed

--- Solutions/FMCFun.hs
+++ Solutions/FMCFun.hs
@@ -13,7 +13,7 @@ data Nat where
     O :: Nat
     S :: Nat -> Nat
 
-zero, one, two, three, four, five, six, seven, eight, nine, false, true :: Nat
+zero, one, two, three, four, five, six, seven, eight, nine :: Nat
 zero  = O
 one   = S zero
 two   = S one
@@ -27,13 +27,13 @@ nine  = S eight
 
 -- curry takes a "traditional" binary function
 -- and returns its currified version
-curry :: undefined
-curry = undefined
+curry :: ((x, y) -> z) -> x -> y -> z
+curry f x y = f (x, y)
 
 -- uncurry takes a currified function
 -- and returns its "traditional" binary version
-uncurry :: undefined
-uncurry = undefined
+uncurry :: (x -> y -> z) -> (x, y) -> z
+uncurry f (x, y) = f x y
 
 -- flip takes a (currified) binary function
 -- and returns one that behaves the same but takes its arguments in the opposite order
@@ -41,15 +41,26 @@ uncurry = undefined
 -- (.) takes two composable functions and returns their composition
 
 -- (.>) is composition but in diagramatic notation (should be ; but Haskell forbids)
+
+flip :: (x -> y -> z) -> y -> x -> z
+flip f x y = f y x
+ 
+(.) :: (y -> z) -> (x -> y) -> x -> z
+(f . g) x = f (g x)
+
+(.>) :: (x -> y) -> (y -> z) -> x -> z
 (.>) = flip (.)
 
 -- ($) takes a function and a suitable argument and applies the function to the argument
 -- think: why would we ever want that?
+($) :: (x -> y) -> x -> y
+f $ x = f x
 
 -- iterate: figure it out by its type
 iterate :: (a -> a) -> a -> [a]
-iterate = undefined
+iterate f x = x : iterate f (f x)
 
 -- orbit
+orbit :: y -> (y -> y) -> [y]
 orbit = flip iterate
 
